f553e04fc8e593d2887bbda04cc6f0912239acf8
Julia Vik Remøy <juliavr@stud.ntnu.no>
2023-07-11 22:40:25 +0200

    Standard files

--- src/main/java/com/example/drinking/game/Main.java
+++ src/main/java/com/example/drinking/game/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.example.drinking.game;
 
 public class Main {
   public static void main(String[] args) {
---
+++ src/main/java/com/example/drinking/game/model/Player.java
@@ -0,0 +1,4 @@
+package com.example.drinking.game.model;
+
+public class Player {
+}
